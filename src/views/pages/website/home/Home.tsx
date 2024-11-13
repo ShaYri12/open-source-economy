aa8b64f90050b1f918bae9c38b6258f44c4bd576
ShaYri12 <sharjeelb60@gmail.com>
2024-11-14 02:44:28 +0500

    done

--- src/views/pages/website/home/Home.tsx
+++ src/views/pages/website/home/Home.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import Sec2 from "./elements/Sec2/Sec2";
 import Sec1 from "./elements/Sec1/Sec1";
 import { PageWrapper } from "../../PageWrapper";
-import "./Home.css";
 import Sec3 from "src/views/pages/website/home/elements/Sec3/Sec3";
 import Sec4 from "src/views/pages/website/home/elements/Sec4/Sec4";
 
